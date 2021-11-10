8ce75d6296271afb46c310ecdf8cfe5005cc1455
Maja <majacvetkovic@outlook.com>
2021-11-10 08:58:43 +0100

    recipe list responsive layout

--- recipes-app/src/components/RecipeList/RecipeList.js
+++ recipes-app/src/components/RecipeList/RecipeList.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import RecipeListCard from "../RecipeListCard/RecipeListCard";
 
 import { retrieveRecipes } from "../../parse/api";
-import { Container } from "@mui/material";
+import { Container, Box } from "@mui/material";
 
 import { useDispatch } from "react-redux";
 import { setRecipeList } from "../../redux/recipeList";
@@ -22,11 +22,13 @@ const RecipeList = () => {
   }, [dispatch]);
 
   return (
-    <Container size="lg" sx={{ pt: 10, display: 'flex', justifyContent: 'center', flexWrap: 'wrap', gap: 5 }}>
-      {list &&
-        list.map((recipe, index) => {
-          return (<RecipeListCard key={index} id={index} recipe={recipe} />);
-        })}
+    <Container size='lg' sx={{ mt: 15 }}>
+      <Box size='lg' display="grid" gridTemplateColumns={{ sm: "repeat(1, 1fr)", lg: "repeat(3, 1fr)" }} gap={5}>
+        {list &&
+          list.map((recipe, index) => {
+            return (<RecipeListCard key={index} id={index} recipe={recipe} />);
+          })}
+      </Box >
     </Container>
   );
 };
--- recipes-app/src/components/RecipeListCard/RecipeListCard.js
+++ recipes-app/src/components/RecipeListCard/RecipeListCard.js
@@ -11,7 +11,7 @@ import {
 } from "@mui/material";
 
 import CheckCircle from "@mui/icons-material/CheckCircle";
-
+import styled from 'styled-components'
 
 const RecipeListCard = ({ recipe }) => {
   const ellipsize = require("ellipsize");
@@ -22,48 +22,65 @@ const RecipeListCard = ({ recipe }) => {
   };
 
   return (
-    <Link to={location} style={{ textDecoration: 'none', color: '#070707', maxWidth: '30%', width: '100%', mr: 'auto' }}>
-      <Card>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            height="160"
-            image="https://images.unsplash.com/photo-1587394214354-a427608f15cc?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjF8fHBhc3RhfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60"
-            alt="pasta"
-          />
-          <CardContent>
-            <Box>
-              <Typography gutterBottom variant="overline" component="h2">
-                {recipe.name}
-              </Typography>
-              <Typography variant="body1" color="text.secondary" component="p">
-                ID:{recipe.id}
-              </Typography>
-              <Box
-                sx={{
-                  display: "flex",
-                  mt: 3,
-                  mb: 1,
-                  gap: 4
-                }}
-              >
-                <Typography variant="body1" color="text.secondary" component="p">
-                  {recipe.source}
+    <Box gridColumn="span 1">
+      <Link to={location} style={{ textDecoration: 'none', color: '#070707', height: '100%' }}>
+        <CardWrapper>
+          <ActionArea>
+            <CardMedia
+              component="img"
+              height="160"
+              image="https://images.unsplash.com/photo-1587394214354-a427608f15cc?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjF8fHBhc3RhfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60"
+              alt="pasta"
+            />
+            <CardContent>
+              <Box>
+                <Typography gutterBottom variant="overline" component="h2">
+                  {recipe.name}
                 </Typography>
                 <Typography variant="body1" color="text.secondary" component="p">
-                  {recipe.time}
+                  ID:{recipe.id}
                 </Typography>
+                <Box
+                  sx={{
+                    display: "flex",
+                    mt: 3,
+                    mb: 1,
+                    gap: 4
+                  }}
+                >
+                  <Typography variant="body1" color="text.secondary" component="p">
+                    {recipe.source}
+                  </Typography>
+                  <Typography variant="body1" color="text.secondary" component="p">
+                    {recipe.time}
+                  </Typography>
+                </Box>
               </Box>
-            </Box>
-            <Box>
-              <Typography gutterBottom variant="body1" component="p">
-                Total ingredients: {recipe.ingredients.length}
-              </Typography>
-              {recipe.ingredients.map((ingredient, index) => {
-                if (recipe.ingredients.length >= 2) {
-                  while (index <= 2) {
+              <Box>
+                <Typography gutterBottom variant="body1" component="p">
+                  Total ingredients: {recipe.ingredients.length}
+                </Typography>
+                {recipe.ingredients.map((ingredient, index) => {
+                  if (recipe.ingredients.length >= 2) {
+                    while (index <= 2) {
+                      return (
+                        <Box key={index}>
+                          <CheckCircle color='success' sx={{ fontSize: 20 }} />
+                          <Typography
+                            gutterBottom
+                            variant="body2"
+                            component="p"
+                            sx={{ mb: 0 }}
+                          >
+                            {ingredient.name}
+                            {index === 2 && '...'}
+                          </Typography>
+                        </Box>
+                      );
+                    }
+                  } else {
                     return (
-                      <Box key={index} sx={{ display: 'flex', alignItems: 'center', gap: 2, mb: 2 }}>
+                      <Box key={index}>
                         <CheckCircle color='success' sx={{ fontSize: 20 }} />
                         <Typography
                           gutterBottom
@@ -72,42 +89,43 @@ const RecipeListCard = ({ recipe }) => {
                           sx={{ mb: 0 }}
                         >
                           {ingredient.name}
-                          {index === 2 && '...'}
                         </Typography>
                       </Box>
                     );
                   }
-                } else {
-                  return (
-                    <Box key={index} sx={{ display: 'flex', alignItems: 'center', gap: 2, mb: 2 }}>
-                      <CheckCircle color='success' sx={{ fontSize: 20 }} />
-                      <Typography
-                        gutterBottom
-                        variant="body2"
-                        component="p"
-                        sx={{ mb: 0 }}
-                      >
-                        {ingredient.name}
-                      </Typography>
-                    </Box>
-                  );
-                }
-              })}
+                })}
 
-            </Box>
-            <Typography variant="body2" color="text.secondary">
-              {ellipsize(recipe.instructions, 50)}
-            </Typography>
-          </CardContent>
+              </Box>
+              <Typography variant="body2" color="text.secondary">
+                {ellipsize(recipe.instructions, 50)}
+              </Typography>
+            </CardContent>
 
-        </CardActionArea>
-      </Card>
-    </Link>
-
+          </ActionArea>
+        </CardWrapper>
+      </Link>
+    </Box>
   );
 };
 
 export default RecipeListCard;
 
 
+const CardWrapper = styled(Card)`
+   {
+    height: 100%; 
+    display: flex; 
+    flex-direction: column; 
+    align-items: flex-start;
+  }
+`;
 
+const ActionArea = styled(CardActionArea)`
+  {
+    height: 100%; 
+    display: flex; 
+    flex-direction: column; 
+    align-items: flex-start;
+    justify-content: flex-start;
+  }
+`;
